f3db4ac24cfaf6f800ce8da860c2bad8bcaf09f7
TamaranaSireesha <sireeshatamarana59@gmail.com>
2023-10-04 12:04:39 +0530

    second commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,14 +15,15 @@ const Login = () => {
   const password=useRef(null);
 
   const HandleValidation=()=>{
-    //checkValidateData(email,password);
-    //console.log(email);
-    // console.log(email.current.value);
+    checkValidateData(email,password);
+    console.log(email);
+   // console.log(password);
+    console.log(email.current.value);
 
-    // console.log(password.current.value);
+    console.log(password.current.value);
 
   const message=  checkValidateData(email.current.value,password.current.value);
-  console.log(message);
+  //console.log(message);
   setErrorMessage(message);
   if(message) return;
   
@@ -47,7 +48,6 @@ const Login = () => {
   }
   else{
     //signin user
- 
  signInWithEmailAndPassword(auth,email.current.value,password.current.value)
   .then((userCredential) => {
     // Signed in 
@@ -59,7 +59,8 @@ const Login = () => {
     const errorCode = error.code;
     const errorMessage = error.message;
     setErrorMessage(errorCode+"-"+errorMessage);
-  });} 
+  });
+} 
 
 
   }
@@ -67,20 +68,26 @@ const Login = () => {
   const toggleclick=()=>{
     setSignInForm(!isSignInform);
   }
+ 
+  
   return (
     <div>
       <Header/>
       <div className ="absolute">
       <img src= "https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-4e98-bdea-5d983e86bf52/b5953637-091d-4e02-9754-2bfadc8a8f7c/IN-en-20230925-popsignuptwoweeks-perspective_alpha_website_large.jpg"  alt='background image'/>
     </div>
-    <form onSubmit={(e)=>email.prevent.default} className="bg-black p-12 absolute  w-3/12  mx-auto my-36 right-0 left-0 bg-opacity-80 rounded-md text-white" onClick={HandleValidation}>
+    <form  onSubmit={(e) => e.preventDefault()} className="bg-black p-12 absolute  w-3/12  mx-auto my-36 right-0 left-0 bg-opacity-80 rounded-md text-white" onClick={HandleValidation}>
 
      <h1 className='font-bold text-2xl text-white  m-2 '>{isSignInform ?"Sign In":"Sign Up"}</h1> 
       {!isSignInform &&<input type="text" placeholder='Full Name' className="p-3 my-3 w-full  bg-gray-700"/>}
 
-     <input type="text" placeholder='Email or phone number' className="p-3 my-3 w-full bg-gray-700 " ref={email}/>
-
-     <input type="password" placeholder='password' className="p-3 my-3 w-full  bg-gray-700 " ref={password}/>
+     <input type="text" placeholder='Email or phone number'  className="p-3 my-3 w-full bg-gray-700 " ref={email}/>
+     <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
      <p className='text-red-600'>{ErrorMessage}</p>
      <button className="text-white p-3 my-6 bg-red-700 w-full rounded-md">{ isSignInform ?"Sign In":"Sign Up"}</button>
      <p className='text-white cursor-pointer py-4' onClick={toggleclick}>{isSignInform?"New to Netflix? Sign up now.":"Already Registed? Sign In."}</p>
--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -1,6 +1,6 @@
  export const  checkValidateData=(email,password)=>{
    const emailvalid=/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email) ;
-   const passwordvalid=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})$/.test(password);
+   const passwordvalid=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).{8,10}$/.test(password);
    if(!emailvalid) return "Email Id is not valid";
    if(!passwordvalid) return "Password Id is not valid";
    return null ; 
